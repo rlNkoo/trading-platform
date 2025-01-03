361d7c4adc9de816fe7745822b107b03bb0391c7
Artur Rolak <rolakartur96@gmail.com>
2025-01-03 20:23:18 +0100

    CoinService started

---
+++ src/main/java/com/rlnkoo/service/CoinService.java
@@ -0,0 +1,22 @@
+package com.rlnkoo.service;
+
+import com.rlnkoo.model.Coin;
+
+import java.util.List;
+
+public interface CoinService {
+
+    List<Coin> getCoinList(int page) throws Exception;
+
+    String getMartketChart(String coinId, int days) throws Exception;
+
+    String getCoinDetails(String coinId) throws Exception;
+
+    Coin findById(String coinId) throws Exception;
+
+    String searchCoin(String keyword);
+
+    String getTop50CoinsByMarketCapRank();
+
+    String getTredingCoins();
+}
