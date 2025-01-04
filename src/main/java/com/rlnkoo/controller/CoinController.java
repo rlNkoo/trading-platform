2c90cef21f01c778871e95bc372bd02333d6cc24
Artur Rolak <rolakartur96@gmail.com>
2025-01-04 11:16:25 +0100

    CoinController finished

--- src/main/java/com/rlnkoo/controller/CoinController.java
+++ src/main/java/com/rlnkoo/controller/CoinController.java
@@ -1,10 +1,15 @@
 package com.rlnkoo.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rlnkoo.model.Coin;
 import com.rlnkoo.service.CoinService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/coins")
@@ -15,4 +20,52 @@ public class CoinController {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @GetMapping
+    ResponseEntity<List<Coin>> getCoinList(@RequestParam("page") int page) throws Exception {
+        List<Coin> coins = coinService.getCoinList(page);
+
+        return new ResponseEntity<>(coins, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{coinId}/chart")
+    ResponseEntity<JsonNode> getMarketChart(@PathVariable String coinId,
+                                            @RequestParam("days") int days) throws Exception {
+        String response = coinService.getMartketChart(coinId, days);
+        JsonNode jsonNode = objectMapper.readTree(response);
+
+        return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/search")
+    ResponseEntity<JsonNode> searchCoin(@RequestParam("q") String keyword) throws Exception {
+        String coin = coinService.searchCoin(keyword);
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+    }
+
+    @GetMapping("/top50")
+    ResponseEntity<JsonNode> getTop50CoinByMarketCapRank() throws Exception {
+        String coin = coinService.getTop50CoinsByMarketCapRank();
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+    }
+
+    @GetMapping("/treading")
+    ResponseEntity<JsonNode> getTreadingCoin() throws Exception {
+        String coin = coinService.getTredingCoins();
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+    }
+
+    @GetMapping("/details/{coinId}")
+    ResponseEntity<JsonNode> getCoinDetails(@PathVariable String coinId) throws Exception {
+        String coin = coinService.getCoinDetails(coinId);
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+    }
 }
